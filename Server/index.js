5e015d53bf382407c92dcb90a296bd4165d7c87b
Prakhar0804 <140363573+Prakhar0804@users.noreply.github.com>
2024-09-17 18:11:41 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')('https://zenova.netlify.app/', {
+const io = require('socket.io')(8000, {
     cors: {
         origin: "*",  // Allow all origins, modify this for specific domains in production
         methods: ["GET", "POST"]
